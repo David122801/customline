b074597a85b6f781bf6d914f13a4c576de548595
David <durfay122801@gmail.com>
2025-01-21 19:45:59 -0500

    Commit 0.1

---
+++ src/Pages/Personalizar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Personalizar() {
+  return (
+    
+    <div>
+      <h2>Personalización</h2>
+      <p>Aquí puedes personalizar tus productos como desees.</p>
+    </div>
+  );
+}
+
+export default Personalizar;
